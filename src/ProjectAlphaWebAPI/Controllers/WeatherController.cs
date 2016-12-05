a380ec5adc99f59ac09668c11d31e636d2cf5a41
Lee Song <leesong1995@hotmail.com>
2016-12-05 23:12:14 +0800

    Add project files.

---
+++ src/ProjectAlphaWebAPI/Controllers/WeatherController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectAlphaWebAPI.Models;
+
+namespace ProjectAlphaWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+        DatabaseContext db;
+
+        public WeatherController(DatabaseContext context)
+        {
+            db = context;
+        }
+    }
+}
---
+++ src/ProjectAlphaWebAPI/Models/Weather.cs
@@ -0,0 +1,34 @@
+﻿using ProjectAlphaWebAPI.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAlphaWebAPI.Models
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Street { get; set; }
+
+        [StringLength(50)]
+        public string Park { get; set; }
+
+        [StringLength(6)]
+        public string Postal { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [StringLength(50)]
+        public string State { get; set; }
+
+        public DateTimeOffset DateTime { get; set; }
+
+        [DecimalPrecision(3,6)]
+        public double Temperature { get; set; }
+    }
+}
