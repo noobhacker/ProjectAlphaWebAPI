2761f63cebfd8639577f6af40cffed658133ac0a
Lee Song <leesong1995@hotmail.com>
2016-12-06 12:39:16 +0800

    removed template controller and added publish information

--- src/ProjectAlphaWebAPI/Controllers/WeatherController.cs
+++ src/ProjectAlphaWebAPI/Controllers/WeatherController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjectAlphaWebAPI.Models;
+using ProjectAlphaWebAPI.Responses;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjectAlphaWebAPI.Controllers
 {
@@ -17,6 +19,25 @@ namespace ProjectAlphaWebAPI.Controllers
             db = context;
         }
 
-
+        [HttpGet("{keyword}")]
+        public async Task<List<WeatherResponse>> Get(string keyword)
+            => await (from weather in db.Weathers
+                      where weather.Street.Contains(keyword) ||
+                            weather.Park.Contains(keyword) ||
+                            weather.City.Contains(keyword) ||
+                            weather.Postal.Contains(keyword) ||
+                            weather.State.Contains(keyword)
+                      select new WeatherResponse()
+                      {
+                          // the reason i do this is to make conditional tests at database level
+                          bestAccuracyLocationName = weather.State.Contains(keyword) ? weather.State :
+                                                     // don't response postal! response city name
+                                                     weather.Postal.Contains(keyword) ? weather.City :
+                                                     weather.City.Contains(keyword) ? weather.City :
+                                                     weather.Park.Contains(keyword) ? weather.Park :
+                                                     weather.Street.Contains(keyword) ? weather.Street : null,
+                          dateTime = weather.DateTime,
+                          temperature = weather.Temperature
+                      }).ToListAsync();
     }
 }
--- src/ProjectAlphaWebAPI/Responses/WeatherResponse.cs
+++ src/ProjectAlphaWebAPI/Responses/WeatherResponse.cs
@@ -7,11 +7,7 @@ namespace ProjectAlphaWebAPI.Responses
 {
     public class WeatherResponse
     {
-        public string street { get; set; }
-        public string park { get; set; }
-        public string postal { get; set; }
-        public string city { get; set; }
-        public string state { get; set; }
+        public string bestAccuracyLocationName { get; set; }
         public DateTimeOffset dateTime { get; set; }
         public double temperature { get; set; }
     }
